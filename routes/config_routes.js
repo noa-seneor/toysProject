501dcda0d03dba207254c7ef2a246bbe024647e4
noa-seneor <noaseneor@gmail.com>
2023-06-11 21:41:38 +0300

    last commit

--- routes/config_routes.js
+++ routes/config_routes.js
@@ -1,11 +1,9 @@
 const indexR = require("./index");
 const usersR = require("./users");
-const cakesR = require("./cakes");
 const toysR = require("./toys");
 
 exports.routesInit = (app) => {
   app.use("/",indexR);
   app.use("/users",usersR);
-  app.use("/cakes",cakesR);
   app.use("/toys",toysR);
 }
